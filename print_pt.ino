79e1183fd0cd9cf9cbad961a8b1922e815133826
Citeli-py <81636534+Citeli-py@users.noreply.github.com>
2021-10-01 17:03:02 -0300

    Update print_pt.ino

--- print_pt.ino
+++ print_pt.ino
@@ -1,72 +1,50 @@
- 
-#include "Keyboard.h"
- 
-void print_pt(String phrase)
+void print_pt(String frase)
 {
-  int len = phrase.length();
-
-  for ( int i=0; i <= len; i++)
+  char matriz[][3] = {
+            { '^', 0x22, ' ' },
+            { '~', 0x27, ' ' },
+            { ';', 0x2f, '\0' },
+            { ':', 0x3f, '\0' },
+            { ']', 0x5c, '\0' },
+            { '[', 0x5d, '\0' },
+            { '\'', 0x60, '\0'},
+            { '}', 0x7c, '\0' },
+            { '{', 0x7d, '\0' },
+            { '\"', '~', '\0' },
+            { '?' , 'R', 'w'  },
+            { '/' , 'R', 0x71 }};
+            
+  int len = 12;
+  for ( int i=0; i <= frase.length(); i++)
   {
-    switch(phrase[i])
+    int j; //Criei fora do for para que a variável j exista fora do loop
+    for (j = 0; j < len; j++)
+    {
+      if (frase[i] == matriz[j][0])
+        break;
+    }
+
+    if (j < len) //Se j < 10 então o for foi interrompido pelo break
+    {
+      //Encontrei na tabela -> Imprimir o valor convertido
+      if(matriz[j][1] != 'R') // R -> é para utilizar um comando com ALT + alguma tecla
       {
-        case '^':
-          Keyboard.write(0x22);
-          Keyboard.write(' ');
-          break;
-  
-        case '~':
-          Keyboard.write(0x27);
-          Keyboard.write(' ');
-          break;
-  
-        case ';':
-          Keyboard.write(0x2f);
-          break;
-  
-        case ':':
-          Keyboard.write(0x3f);
-          break;
-        
-        case ']':
-          Keyboard.write(0x5c);
-          break;
-  
-        case '[':
-          Keyboard.write(0x5d);
-          break;
-  
-        case '\'':
-          Keyboard.write(0x60);
-          break;
-  
-        case '}':
-          Keyboard.write(0x7c);
-          break;
-  
-        case '{':
-          Keyboard.write(0x7d);
-          break;
-  
-        case '\"':
-          Keyboard.write('~');
-          break;
-  
-        case '?':
-          Keyboard.press(KEY_RIGHT_ALT);
-          Keyboard.press('w');
-          Keyboard.releaseAll();
-          break;
-  
-        case '/':
-          Keyboard.press(KEY_RIGHT_ALT);
-          Keyboard.press(0x71);
-          Keyboard.releaseAll();
-          break;
-  
-        default:
-          Keyboard.write(phrase[i]);
-          break;
-      
+        Keyboard.write(matriz[j][1]);
+        if (matriz[j][2] != '\0')
+          Keyboard.write(matriz[j][2]);
       }
+      else // Caso não possua R só digite
+      {
+        Keyboard.press(KEY_RIGHT_ALT);
+        Keyboard.write(matriz[j][2]);
+        Keyboard.releaseAll(); 
+      }
+    }
+    else //Caso contrário, toda a matriz foi percorrida sem nenhuma correspondência
+    {
+      //Não encontrei na tabela -> Imprimir o valor original
+      Keyboard.write(frase[i]);
+    }
+    
   }
 }
